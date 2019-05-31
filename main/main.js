20e6f7bf82c036ab1503e102b1fb6e4218a5b90b
yihuan18 <18200120142@163.com>
2019-05-31 15:11:27 +0800

    出租计价

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var distanceCost = 0;
+    var parkCost = inputs.parkTime * 0.25;
+    var totalCost = 0;
+    if(inputs.distance <= 2){
+        distanceCost = 6;
+    }else if(inputs.distance <= 8){
+        distanceCost = 6 + (inputs.distance - 2) * 0.8;
+    }else{
+        distanceCost = 6 + 6 * 0.8 + (inputs.distance - 8) * 0.8 * 1.5;
+    }
+
+    totalCost = Math.round(distanceCost + parkCost);
+
+    return totalCost;
 };
